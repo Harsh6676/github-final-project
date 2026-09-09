9d5ce846fb169cf6588e2d8e91be75297493265a
Harsh6676 <harshchandan77@gmail.com>
2026-09-09 19:42:09 +0530

    Add simple interest calculation script

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter the principal:"
+read p
+
+echo "Enter rate of interest per year:"
+read r
+
+echo "Enter time period in years:"
+read t
+
+s=$(echo "scale=2; $p * $r * $t / 100" | bc)
+
+echo "The simple interest is: $s"
